98ba10f7948c7d949dc7cb879c94bdc3580d989c
kevinfiol <fiolkevin@gmail.com>
2020-01-24 16:41:55 -0500

    add del

--- src/clean.js
+++ src/clean.js
@@ -1,2 +1,24 @@
-const rimraf = require('rimraf');
-rimraf('./_site/*', () => console.log('cleaned _site'));
+// const rimraf = require('rimraf');
+// const globby = require('globby');
+
+// globby(['_site/**', '!_site/bear/', '!_site/teddy.txt'])
+//     .then(paths => {
+//         paths.map(item => {
+//             rimraf.sync(item);
+//         });
+//     })
+// ;
+
+const del = require('del');
+
+// del.sync([
+//     '_site/**',
+//     '!_site/bear',
+//     '!_site/teddy.txt'
+// ]);
+
+// del.sync([
+//     '../**',
+//     '../.git',
+//     '../src'
+// ]);
