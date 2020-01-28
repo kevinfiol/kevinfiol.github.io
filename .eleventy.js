a51a872ae7ee551030701e707c9b703be5e014c6
kevinfiol <fiolkevin@gmail.com>
2020-01-28 00:19:18 -0500

    netlify me

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/css");
-    eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPlugin(syntaxHighlight);
 };
--- copy.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs-extra');
-
-fs.copy('./_site', './', e => {
-    if (e) return console.error(e);
-    console.log('copied _site to current dir');
-});
