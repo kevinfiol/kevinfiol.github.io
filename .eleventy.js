bcbac7604e7c70eb7d1ad321beab6904a2cb5237
kevinfiol <fiolkevin@gmail.com>
2020-01-31 16:03:35 -0500

    add 404

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,6 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("_redirects");
     eleventyConfig.addPlugin(syntaxHighlight);
 };
