bebfdd0e48c1876d8e60cb7a94cc99c2e10c6819
kevinfiol <fiolkevin@gmail.com>
2020-02-26 00:02:03 -0500

    add dark mode

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("js");
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("_redirects");
     eleventyConfig.addPlugin(syntaxHighlight);
